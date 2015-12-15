c44339079bfc24c23dc4ea327cb7a260750f4479
Tomasz Wychocki <wychoniu@gmail.com>
2015-12-15 21:00:38 +0100

    Dodano obsługe pełnego ekranu

--- Tor/Tor/Form1.Designer.cs
+++ Tor/Tor/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -37,12 +39,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
@@ -56,6 +61,26 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Ustawienia";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(160, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 20);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label3
             // 
@@ -132,19 +157,31 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(594, 429);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Koniec";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(681, 464);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Droga na tarczy";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -163,6 +200,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Tor/Tor/Form1.cs
+++ Tor/Tor/Form1.cs
@@ -17,8 +17,7 @@ namespace Tor
         public Form1()
         {
             InitializeComponent();
-            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
-            pictureBox1.Image = DrawArea;
+            this.WindowState = FormWindowState.Maximized;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -30,44 +29,50 @@ namespace Tor
         {
             Graphics g;
             g = Graphics.FromImage(DrawArea);
-            g.Clear(Color.White);
+            g.Clear(Color.Gray);
             Pen mypen = new Pen(Brushes.Black);
-            g.DrawLine(mypen, 0, 220, 440, 220);
-            g.DrawLine(mypen, 220, 0, 220, 440);
+            g.DrawLine(mypen, 0, pictureBox1.Size.Width/2, pictureBox1.Size.Width, pictureBox1.Size.Width/2);
+            g.DrawLine(mypen, pictureBox1.Size.Width / 2, 0, pictureBox1.Size.Width / 2, pictureBox1.Size.Width);
             pictureBox1.Image = DrawArea;
             g.Dispose();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Draw();
+        }
+
+        private void Draw()
+        {
             Graphics g;
             g = Graphics.FromImage(DrawArea);
-            Pen mypen = new Pen(Brushes.Black);
+            Pen RedPen = new Pen(Brushes.Red, 3);
             reset();
 
             float r = Convert.ToSingle(textBox1.Text) * metr;
             float omega = Convert.ToSingle(textBox2.Text);
             float v = Convert.ToSingle(textBox3.Text) * metr;
+            Point[] curvePoints = new Point[10000];
+            int i = 0;
             bool two = false;
-            g.DrawEllipse(mypen, 220 - (r / 2), 220 - (r / 2), r, r);
+            g.FillEllipse(new SolidBrush(Color.LightGray), (pictureBox1.Size.Width / 2) - (r / 2), (pictureBox1.Size.Width / 2) - (r / 2), r, r);
             //-------------------------------------------------------
             double kat = 90;
+            curvePoints[i] = new Point(
+                Convert.ToInt32((r / 2 * Math.Cos(kat * Math.PI / 180)) + (pictureBox1.Size.Width / 2)), 
+                Convert.ToInt32((r / 2 * Math.Sin(kat * Math.PI / 180)) + (pictureBox1.Size.Width / 2))
+                );
+            i++;
 
             do
             {
-                SetPixel(
-                    g, 
-                    Convert.ToInt32((r / 2 * Math.Cos(kat * Math.PI / 180)) + 220), 
-                    Convert.ToInt32((r / 2 * Math.Sin(kat * Math.PI / 180)) + 220)
-                    );
-
                 if (!two)
                     r -= v;
                 else
                     r += v;
 
                 kat += omega;
-                if (kat > 360)
+                if (kat == 360)
                     kat = 0;
 
                 if (r <= 0)
@@ -76,16 +81,43 @@ namespace Tor
                     r = 0;
                 }
 
-            }
-            while (r != Convert.ToInt32(textBox1.Text) * metr);
+                curvePoints[i] = new Point(
+                    Convert.ToInt32((r / 2 * Math.Cos(kat * Math.PI / 180)) + (pictureBox1.Size.Width / 2)),
+                    Convert.ToInt32((r / 2 * Math.Sin(kat * Math.PI / 180)) + (pictureBox1.Size.Width / 2))
+                    );
+                i++;
 
+            }
+            while (r < Convert.ToInt32(textBox1.Text) * metr);
+
+            g.DrawCurve(RedPen, curvePoints);
             pictureBox1.Image = DrawArea;
             g.Dispose();
         }
 
-        private void SetPixel(Graphics g, int x, int y)
+        private void button2_Click(object sender, EventArgs e)
         {
-            g.FillRectangle(Brushes.Black, new Rectangle(x, y, 1, 1));
+            textBox2.Text = Convert.ToString(Convert.ToDouble(textBox2.Text) + 0.1);
+            Draw();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Convert.ToString(Convert.ToDouble(textBox2.Text) - 0.1);
+            Draw();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(Size.Width - 241, Size.Height - 24);
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = DrawArea;
+            groupBox1.Location = new Point(Size.Width - 223, 12);
         }
     }
 }
