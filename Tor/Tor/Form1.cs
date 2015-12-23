1132d606bdc90007cdd61d5190137f9ec91a9342
Tomasz Wychocki <wychoniu@gmail.com>
2015-12-23 21:57:19 +0100

    Wszystko dziala

--- Tor/Tor/Form1.cs
+++ Tor/Tor/Form1.cs
@@ -51,17 +51,15 @@ namespace Tor
             float r = Convert.ToSingle(textBox1.Text) * metr;
             float omega = Convert.ToSingle(textBox2.Text);
             float v = Convert.ToSingle(textBox3.Text) * metr;
-            Point[] curvePoints = new Point[10000];
-            int i = 0;
+            List<Point> pointList = new List<Point>();
             bool two = false;
             g.FillEllipse(new SolidBrush(Color.LightGray), (pictureBox1.Size.Width / 2) - (r / 2), (pictureBox1.Size.Height / 2) - (r / 2), r, r);
             //-------------------------------------------------------
             double kat = 90;
-            curvePoints[i] = new Point(
+            pointList.Add(new Point(
                 Convert.ToInt32((r / 2 * Math.Cos(kat * Math.PI / 180)) + (pictureBox1.Size.Width / 2)), 
                 Convert.ToInt32((r / 2 * Math.Sin(kat * Math.PI / 180)) + (pictureBox1.Size.Height / 2))
-                );
-            i++;
+                ));
 
             do
             {
@@ -80,20 +78,21 @@ namespace Tor
                     r = 0;
                 }
 
-                curvePoints[i] = new Point(
+                pointList.Add(new Point(
                     Convert.ToInt32((r / 2 * Math.Cos(kat * Math.PI / 180)) + (pictureBox1.Size.Width / 2)),
                     Convert.ToInt32((r / 2 * Math.Sin(kat * Math.PI / 180)) + (pictureBox1.Size.Height / 2))
-                    );
-                i++;
-
+                    ));
             }
             while (r < Convert.ToInt32(textBox1.Text) * metr);
 
-            g.DrawCurve(RedPen, curvePoints);
+            Point[] pointArray = pointList.ToArray();
+            g.DrawCurve(RedPen, pointArray);
+
             Pen mypen = new Pen(Brushes.Black);
             g.DrawLine(mypen, 0, pictureBox1.Size.Height / 2, pictureBox1.Size.Width, pictureBox1.Size.Height / 2);
             g.DrawLine(mypen, pictureBox1.Size.Width / 2, 0, pictureBox1.Size.Width / 2, pictureBox1.Size.Height);
             pictureBox1.Image = DrawArea;
+
             g.Dispose();
         }
 
